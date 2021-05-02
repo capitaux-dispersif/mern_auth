b63b34ce4f6582544b606825c50effe42460ace0
Test123 <Test123@Gavins-MacBook-Pro.local>
2021-05-02 18:40:38 -0400

    fixing running errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import { Component } from 'react';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
-    </Provider>
-  );
-}
+// function App() {
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  };
+};
 
 export default App;
